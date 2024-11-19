a34a68049e5011785f39236decceb99ffd4414a1
Ashish8242 <ashishsharma824236@gmail.com>
2024-11-19 23:55:34 +0530

    4

---
+++ checker.java
@@ -0,0 +1,12 @@
+package exp_4_4;
+
+import java.util.Scanner;
+
+public class checker {
+public static void main(String[] args) {
+	Scanner in = new Scanner(System.in);
+	System.out.println("enter the line");
+	String st = in.nextLine();
+	
+}
+}
