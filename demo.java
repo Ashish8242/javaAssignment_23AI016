066e04ee1b9385c0033cb6c2e6873ae7c92d9940
Ashish8242 <ashishsharma824236@gmail.com>
2024-11-19 23:51:02 +0530

    3_3

--- demo.java
+++ demo.java
@@ -1,29 +1,50 @@
-package exception_handeling;
+package exp_3_3;
+
+class X {
+    int i;
+    int j;
+
+    public X(int i, int j) {
+        this.i = i;
+        this.j = j;
+    }
+
+    public int finalSum() {
+        return i + j;
+    }
+}
+
+class Y extends X {
+
+    public Y(int i, int j) {
+        super(i, j);
+    }
+
+    public int findProduct() {
+        return i * j;
+    }
+}
+
+class Z extends Y {
+
+    public Z(int i, int j) {
+        super(i, j);
+    }
+
+    public int findDifference() {
+        return i - j;
+    }
+}
 
 public class demo {
-  public static void main(String[] args) {
-	  try {
-		  String s = args[0];
-		  int i = Integer.parseInt(s);
-		  int j = i/(i-9);
-		  System.out.println("Value of j" +j);
-		  
-	  }
-	  catch(ArrayIndexOutOfBoundsException ex) {
-		  System.out.println("command line argument is not passed");
-		  
-	  }
-	  catch(NumberFormatException ex) {
-		  System.out.println("string to int conversion");
-	  }
-	  catch(ArithmeticException ex) 
-	   {
-		  System.out.println("Divide by zero");
-	  }
-	  finally {
-		  System.out.println("gsv rocks");
-	  }
-	  System.out.println("Main end");
-  }
-  
+    public static void main(String[] args) {
+        Z obj = new Z(4, 7);
+        int sum = obj.finalSum();
+        int product = obj.findProduct();
+        int difference = obj.findDifference();
+
+        System.out.println("Sum: " + sum);
+        System.out.println("Product: " + product);
+        System.out.println("Difference: " + difference);
+    }
 }
